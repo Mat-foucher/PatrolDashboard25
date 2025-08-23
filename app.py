29a3f9e09af93f718dab7b7670b019cedebcb223
Mathieu Foucher <mfoucher@snowbird.com>
2025-08-23 12:22:06 -0600

    added main

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ import streamlit as st
 from scraper import get_live_data
 from utils import format_time_column, plot_graph
 
-st.set_page_config(page_title="Snowbird Patrol Dashboard", layout="wide")
+def main():
+    st.set_page_config(page_title="Snowbird Patrol Dashboard", layout="wide")
 
-st.title("Snowbird Patrol Dashboard")
+    st.title("Snowbird Patrol Dashboard")
 
-df = get_live_data()
-df = format_time_column(df)
+    df = get_live_data()
+    df = format_time_column(df)
 
-st.markdown(f"**Last Updated:** {df['DATETIME'].max()}")
+    st.markdown(f"**Last Updated:** {df['DATETIME'].max()}")
 
-plot_graph(df)
+    plot_graph(df)
+
+if __name__ == '__main__':
+    main()
