57991c03ebe605ca870b8c209cf3d3c576358950
Mathieu Foucher <mfoucher@snowbird.com>
2025-08-24 14:48:19 -0600

    no need for format time column function

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 # Dependencies:
 import streamlit as st
+import time
 from scraper import get_live_data
 from utils import format_time_column, plot_base_graph
 
 def main():
     st.set_page_config(page_title="Snowbird Patrol Dashboard", layout="wide")
 
+    st.autorefresh(interval=600000, key="datarefresh")
+
+    # Page UI:
     st.title("Snowbird Patrol Dashboard")
 
     df = get_live_data()
-    df = format_time_column(df)
+    #df = format_time_column(df)
 
     st.markdown(f"**Last Updated:** {df['DATETIME'].max()}")
 
