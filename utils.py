9f09bbcd619f75efb224ec4b98498d2f6daead8c
Mathieu Foucher <mfoucher@snowbird.com>
2025-08-24 14:33:42 -0600

    use figure from colab notebook

--- utils.py
+++ utils.py
@@ -4,10 +4,31 @@ import plotly.express as px
 import streamlit as st 
 
 def format_time_column(df):
-    df['DATETIME'] = pd.to_datetime(df['TIME'], format="%H%M")
+    #df['DATETIME'] = pd.to_datetime(df['DATETIME'], format="%H%M")
     df = df.sort_values('DATETIME')
     return df 
 
 def plot_base_graph(df):
-    fig = px.line(df, x="DATETIME", y="BASE_TEMP", markers=True)
+    #fig = px.line(df, x="DATETIME", y="BASE_TEMP", markers=True)
+
+    # Base Temp Fig:
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=df['DATETIME'],
+        y=df['BASE_TEMP'],
+        mode='lines+markers',
+        name='Base Temp',
+        line=dict(color='royalblue'),
+        marker=dict(size=4)
+    ))
+
+    fig.update_layout(
+        title="Base Temp (F) Two Day Report",
+        xaxis_title="Datetime",
+        yaxis_title="Temperature (F)",
+        template='plotly_dark',
+        hovermode='x unified'
+    )
+
     st.plotly_chart(fig, use_container_width=True)
