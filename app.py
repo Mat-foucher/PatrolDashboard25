370ad2acd8a20e211f6e743f283f4452f61ec1cf
Mathieu Foucher <mfoucher@snowbird.com>
2025-09-02 21:25:51 -0600

    password protection

--- app.py
+++ app.py
@@ -3,55 +3,69 @@ import streamlit as st
 from scraper import get_live_data
 from utils import format_time_column, plot_base_graph, plot_rose_graph
 from gyro_component import gyro_heading
+import os
 
 def main(option='PEAK'):
-    st.set_page_config(page_title="Snowbird Patrol Dashboard", layout="wide")
 
-    # Call the gyro detection:
-    gyro_heading()
+    # Password Protection:
 
-    # Page UI:
-    df = get_live_data()
-    df = format_time_column(df)
+    stored_password = os.environ.get('PASSWORD')
 
-    # For the elements to be less sparse:
-    g = st.container()
-    
-    # layout - make sure each element is declared in the order you want them to be on the dashboard!!!:
-    col1, col2 = st.columns(2)
-    c = st.container()
-    
-    with g:
-        with col1:
-            st.title("Snowbird Patrol Dashboard (UNOFFICIAL)")
-            st.markdown(f"**Last Updated:** {df['DATETIME'].max()}")
+    pass_input = st.sidebar.text_input("Enter Password:", type="password")
 
-            c1,c2,c3,c4,c5 = st.columns(5)
-
-            # with c1:
-            #     st.metric("Base Temp",df['BASE_TEMP'].iloc[len(df['BASE_TEMP']-1)])
-            # with c2:
-            #     st.metric("Sinners Temp",df['SINN_TEMP'].iloc[len(df['SINN_TEMP']-1)])
-            # with c3:
-            #     st.metric("Peak Temp",df['PEAK_TEMP'].iloc[len(df['PEAK_TEMP']-1)])
-            # with c4:
-            #     st.metric("Gad Temp",df['GAD2_TEMP'].iloc[len(df['GAD2_TEMP']-1)])
-            # with c5:
-            #     st.metric("Redstack Temp:", df['REDSTACK_TEMP'].iloc[len(df['REDSTACK_TEMP']-1)])
-
-        with col2:
-            # Selection Button:
-            option = st.selectbox(
-                "Choose Weather Station:",
-                ("PEAK", "REDSTACK")
-            )
-            plot_rose_graph(df,option)
-
+    if pass_input == stored_password:
         
 
+        st.set_page_config(page_title="Snowbird Patrol Dashboard", layout="wide")
 
-        with c:
-            plot_base_graph(df)
+        # Call the gyro detection:
+        gyro_heading()
+
+        # Page UI:
+        df = get_live_data()
+        df = format_time_column(df)
+
+        # For the elements to be less sparse:
+        g = st.container()
+        
+        # layout - make sure each element is declared in the order you want them to be on the dashboard!!!:
+        col1, col2 = st.columns(2)
+        c = st.container()
+        
+        with g:
+            with col1:
+                st.title("Snowbird Patrol Dashboard (UNOFFICIAL)")
+                st.markdown(f"**Last Updated:** {df['DATETIME'].max()}")
+
+                # c1,c2,c3,c4,c5 = st.columns(5)
+
+                # with c1:
+                #     st.metric("Base Temp",df['BASE_TEMP'].iloc[len(df['BASE_TEMP']-1)])
+                # with c2:
+                #     st.metric("Sinners Temp",df['SINN_TEMP'].iloc[len(df['SINN_TEMP']-1)])
+                # with c3:
+                #     st.metric("Peak Temp",df['PEAK_TEMP'].iloc[len(df['PEAK_TEMP']-1)])
+                # with c4:
+                #     st.metric("Gad Temp",df['GAD2_TEMP'].iloc[len(df['GAD2_TEMP']-1)])
+                # with c5:
+                #     st.metric("Redstack Temp:", df['REDSTACK_TEMP'].iloc[len(df['REDSTACK_TEMP']-1)])
+
+            with col2:
+                # Selection Button:
+                option = st.selectbox(
+                    "Choose Weather Station:",
+                    ("PEAK", "REDSTACK")
+                )
+                plot_rose_graph(df,option)
+
+            
+
+
+            with c:
+                plot_base_graph(df)
+    else:
+        st.error("Incorrect Password")
+        st.warning("Please enter the correct password")
 
 if __name__ == '__main__':
     main()
