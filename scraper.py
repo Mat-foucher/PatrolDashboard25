41f7c35cc8dfa59659f1a0da775b30c89e271fc6
Mathieu Foucher <mfoucher@snowbird.com>
2025-11-25 09:16:18 -0700

    updated wind alert logic since it was sending alerts and danger alerts

--- scraper.py
+++ scraper.py
@@ -265,14 +265,14 @@ def get_live_data(dummy_buster = None):
     max_peak_wind = int(latest_df['PEAK_WIND'].iloc[0])
     max_redstack_wind = int(latest_df['REDSTACK_WIND'].iloc[0])
     
-    if max_peak_wind > 20:
+    if max_peak_wind > 20 and max_peak_wind < 40:
         send_wind_alert(f"ALERT: PEAK WINDS OVER 20 MPH [LAST RECORDED: {max_peak_wind}]") 
-    if max_redstack_wind > 20:
+    if max_redstack_wind > 20 and max_redstack_wind < 40:
         send_wind_alert(f"ALERT: REDSTACK WINDS OVER 20 MPH [LAST RECORDED: {max_redstack_wind}]") 
 
-    if max_peak_wind > 40:
+    if max_peak_wind >= 40:
         send_wind_alert(f"DANGER: PEAK WINDS OVER 40 MPH [LAST RECORDED: {max_peak_wind}]. STAY CLEAR OF RIDGELINES. @Mathieu Foucher") 
-    if max_redstack_wind > 40:
+    if max_redstack_wind >= 40:
         send_wind_alert(f"DANGER: REDSTACK WINDS OVER 40 MPH [LAST RECORDED: {max_redstack_wind}]. STAY CLEAR OF RIDGELINES. @Mathieu Foucher") 
 
     #brdf.to_csv('data/latest.csv', index=False)
